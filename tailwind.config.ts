3d913499f17e84cb59856d33f32efd0a724b4d25
Bryan Atista Kiely <brytista@gmail.com>
2024-10-17 04:02:17 +0800

    feat: add new font weight

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontWeight: {
+        book: "350",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":

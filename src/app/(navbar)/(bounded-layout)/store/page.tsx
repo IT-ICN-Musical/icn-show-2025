fd5af9ae0ae981c5bb46c1f1f4959f944f1ecf27
Bryan Atista Kiely <brytista@gmail.com>
2024-10-17 05:23:44 +0800

    refactor: remove unused codes in store page

--- src/app/(navbar)/(bounded-layout)/store/page.tsx
+++ src/app/(navbar)/(bounded-layout)/store/page.tsx
@@ -6,20 +6,11 @@ import Typography from "@/components/typography/typography";
 import { Button } from "@/components/ui/button";
 
 export default async function Shop() {
-  const show = await fetchShowDetails("8365ade3-b1b0-4bbb-8801-a8eefbcffa58");
-  const clothing = await fetchClothingDetails(
-    "508d4c64-2528-4298-8584-828431144cd9",
-  );
-
   return (
     <main>
       <Typography variant="h3" className="font-safira-march mb-2">
         Store
       </Typography>
-
-      <ClothingSelection clothing={clothing}>
-        <Button>Open Drawer</Button>
-      </ClothingSelection>
     </main>
   );
 }

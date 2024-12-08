2cefb6c69436be3acacd9f2c12d045c49b384791
Clayton Fernalo <clafernalo@gmail.com>
2024-12-08 23:32:52 +0800

    feat: update order preview type

--- src/types/preview-order.ts
+++ src/types/preview-order.ts
@@ -11,33 +11,51 @@ type BundleItem = {
   quantity: number;
 };
 
-type ItemOrderPreview = {
+interface itemOrderPreviewCommonDetails {
   item_id: string;
   name: string;
   image_url?: string;
   quantity: number;
   item_available: boolean;
-  old_unit_price?: number;
+
   unit_price: number;
+
   subtotal: number;
-} & (
-  | {
-      // Ticket additional details
-      start_time?: number; // epoch timestamp
-      end_time?: number; // epoch timestamp
-      is_selling_fast?: boolean;
-    }
-  | {
-      // Clothing additional details
-      size?: string;
-    }
-  | {
-      bundle_items: BundleItem[];
-    }
-);
+}
+
+interface itemOrderPreviewCommonDetailsPromo
+  extends itemOrderPreviewCommonDetails {
+  old_unit_price: number;
+  old_subtotal: number;
+}
+
+type itemOrderPreviewTicketDetails = {
+  start_time: number;
+  end_time: number;
+  is_selling_fast: boolean;
+};
+
+type itemOrderPreviewClothingDetails = {
+  size: string;
+};
+
+type itemOrderPreviewBundleDetails = {
+  bundle_items: BundleItem[];
+};
+
+type ItemOrderPreview = (
+  | itemOrderPreviewCommonDetails
+  | itemOrderPreviewCommonDetailsPromo
+) &
+  (
+    | itemOrderPreviewTicketDetails
+    | itemOrderPreviewClothingDetails
+    | itemOrderPreviewBundleDetails
+  );
 
 export type PreviewOrderResponse = {
   total: number;
+  old_total?: number;
   promo_available: boolean;
   items: ItemOrderPreview[];
 };

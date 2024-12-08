ab58b1856703f0848c2d14576731008ca5237338
Clayton Fernalo <clafernalo@gmail.com>
2024-12-08 18:44:21 +0800

    feat: add metadata for clothing and tickets

--- src/types/preview-order.ts
+++ src/types/preview-order.ts
@@ -1,30 +1,44 @@
 import { CheckoutItemOrderRequest } from "./checkout";
 
+export type PreviewOrderRequest = {
+  promo_code?: string;
+  items: CheckoutItemOrderRequest[]; // Note: orderDto.ItemOrderRequest needs to be defined separately
+};
+
 type BundleItem = {
-  itemId: string;
+  item_id: string;
   name: string;
   quantity: number;
 };
 
 type ItemOrderPreview = {
-  itemId: string;
-  bundleItems?: BundleItem[];
-  name: string;
-  imageUrl?: string;
-  quantity: number;
-  itemAvailable: boolean;
-  oldUnitPrice?: number;
-  unitPrice: number;
-  subtotal: number;
-};
+  item_id: string;
 
-export type PreviewOrderRequest = {
-  promo_code?: string;
-  items: CheckoutItemOrderRequest[];
-};
+  name: string;
+  image_url?: string;
+  quantity: number;
+  item_available: boolean;
+  old_unit_price?: number;
+  unit_price: number;
+  subtotal: number;
+} & (
+  | {
+      // Ticket additional details
+      start_time?: number; // epoch timestamp
+      end_time?: number; // epoch timestamp
+      is_selling_fast?: boolean;
+    }
+  | {
+      // Clothing additional details
+      size?: string;
+    }
+  | {
+      bundle_items?: BundleItem[];
+    }
+);
 
 export type PreviewOrderResponse = {
   total: number;
-  promoAvailable: boolean;
+  promo_available: boolean;
   items: ItemOrderPreview[];
 };

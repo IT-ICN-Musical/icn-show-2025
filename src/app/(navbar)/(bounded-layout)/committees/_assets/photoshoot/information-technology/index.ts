814ce2b071a5bb9a76573d3f209fae41edd9e1a7
Clayton Fernalo <clafernalo@gmail.com>
2025-02-10 22:57:04 +0800

    fix: alphabetize

--- src/app/(navbar)/(bounded-layout)/committees/_assets/photoshoot/information-technology/index.ts
+++ src/app/(navbar)/(bounded-layout)/committees/_assets/photoshoot/information-technology/index.ts
@@ -4,7 +4,7 @@ import BryanAtistaKiely from "./bryan-atista-kiely.jpg";
 import ClaytonFernalo from "./clayton-fernalo.jpg";
 import ElvinSugianto from "./elvin-sugianto.jpg";
 import EmmanuelMatthewChristopherLoho from "./emmanuel-matthew-christopher-loho.jpg";
-import GoodwillBrianLion from "./goodwill-brian-lion.jpg";
+import GoodwillBryanLion from "./goodwill-brian-lion.jpg";
 import ITImage from "./it.jpg";
 import JoshuaJames from "./joshua-james.jpg";
 import JuanFrederick from "./juan-frederick.jpg";
@@ -18,19 +18,42 @@ const InformationTechnologyCommittee: Committee = {
   name: "Information Technology",
   image_src: ITImage.src,
   members: [
-    // Main Committee
     {
       name: "Clayton Fernalo",
       image_src: ClaytonFernalo.src,
       role: "Main Committee",
       course: "CSC/3",
     },
-    // Other Members
     {
       name: "Bryan Atista Kiely",
       image_src: BryanAtistaKiely.src,
       role: "",
       course: "DSAI/3",
+    },
+
+    {
+      name: "Elvin Sugianto",
+      image_src: ElvinSugianto.src,
+      role: "",
+      course: "IEM/3",
+    },
+    {
+      name: "Emmanuel Matthew Christopher Loho",
+      image_src: EmmanuelMatthewChristopherLoho.src,
+      role: "",
+      course: "CE/1",
+    },
+    {
+      name: "Goodwill Bryan Lion",
+      image_src: GoodwillBryanLion.src,
+      role: "",
+      course: "IEM/2",
+    },
+    {
+      name: "Joshua James",
+      image_src: JoshuaJames.src,
+      role: "",
+      course: "EEE/3",
     },
     {
       name: "Juan Frederick",
@@ -45,22 +68,10 @@ const InformationTechnologyCommittee: Committee = {
       course: "IEM/2",
     },
     {
-      name: "Elvin Sugianto",
-      image_src: ElvinSugianto.src,
-      role: "",
-      course: "IEM/3",
-    },
-    {
       name: "Kristian Hadinata Achwan",
       image_src: KristianHadinataAchwan.src,
       role: "",
       course: "MACS/4",
-    },
-    {
-      name: "Emmanuel Matthew Christopher Loho",
-      image_src: EmmanuelMatthewChristopherLoho.src,
-      role: "",
-      course: "CE/1",
     },
     {
       name: "Rafi Bintang Pangestu",
@@ -69,22 +80,10 @@ const InformationTechnologyCommittee: Committee = {
       course: "CBE/4",
     },
     {
-      name: "Goodwill Bryan Lion",
-      image_src: GoodwillBrianLion.src,
-      role: "",
-      course: "IEM/2",
-    },
-    {
       name: "Reswara Anargya Dzakirullah",
       image_src: ReswaraAnargyaDzakirullah.src,
       role: "",
       course: "CSC/2",
-    },
-    {
-      name: "Joshua James",
-      image_src: JoshuaJames.src,
-      role: "",
-      course: "EEE/3",
     },
   ],
 };
